28efe819d1a9d35a86f6fc56bb7b243e85d7994f
Bradston Henry <bradston.henry@gmail.com>
2021-08-24 14:35:30 -0400

    Adding temp url for socket server to react app

--- simple-react-chat-app/src/services/socket.js
+++ simple-react-chat-app/src/services/socket.js
@@ -1,6 +1,6 @@
 import socketIOClient from "socket.io-client";
 
-const serverEndpoint = "https://simple-node-chat-server-bsh.us-south.cf.appdomain.cloud/";
+const serverEndpoint = "http://simple-chat-node-server-a-simple-chat-app.trivia-test-bsh-082721-162e406f043e20da9b0ef0731954a894-0000.us-east.containers.appdomain.cloud/";
 
 export const socket = socketIOClient(serverEndpoint, {
 	transports: ['websocket']
